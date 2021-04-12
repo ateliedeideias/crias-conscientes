790c2dc98097c515c431fa7c731db31d10c9097f
Walber Beltrame <walber.beltrame@gmail.com>
2021-04-12 19:54:56 -0300

    Integraçao com Vue.js

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,20 @@
 <template>
-  <div>About</div>
+  <div class="md-layout md-gutter">
+    <div class="md-center md-layout-item md-size-100">
+      <img alt="Crias Conscientes" src="@/assets/logo.png" width="256" />
+      <p class="md-title">Um game sobre consumo consciente!!</p>
+      <md-button class="md-raised md-accent">Jogue Agora</md-button>
+    </div>
+  </div>
 </template>
 <script>
 export default {};
 </script>
+<style>
+.md-layout {
+  padding: 10px;
+}
+.md-center {
+  text-align: center;
+}
+</style>
