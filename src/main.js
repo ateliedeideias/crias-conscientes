96e3d7903aec75f8e842ec00af2f32cf0f5707b6
Walber Beltrame <walber.beltrame@gmail.com>
2021-04-12 18:36:00 -0300

    Configuraçao de rotas

--- src/main.js
+++ src/main.js
@@ -2,13 +2,18 @@
 import Vue from 'vue';
 import App from './App.vue';
 import VueMeta from 'vue-meta';
+import VueRouter from 'vue-router';
 import VueMaterial from 'vue-material';
 import 'vue-material/dist/vue-material.min.css';
 import 'vue-material/dist/theme/default-dark.css';
 
+import routes from './routes';
+
 Vue.use(VueMeta);
+Vue.use(VueRouter);
 Vue.use(VueMaterial);
 
 new Vue({
- render: h => h(App)
+ render: h => h(App),
+ routes
 }).$mount('#app');
