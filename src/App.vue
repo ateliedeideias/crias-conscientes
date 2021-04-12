a984e9b16d9b4a8d592c43e4da9eaf11c734767e
Walber Beltrame <walber.beltrame@gmail.com>
2021-04-12 18:09:15 -0300

    Integraçao com Vue.js

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <template>
-  <main id="app">
-  </main>
+  <div id="app">
+    <main>
+      <md-tabs md-sync-route>
+        <md-tab id="tab-jogo" md-label="Sobre o jogo" exact></md-tab>
+        <md-tab id="tab-equipe" md-label="A equipe"></md-tab>
+      </md-tabs>
+    </main>
+    <footer></footer>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "App"
+  name: "App",
+  metaInfo: {
+    title: "Crias Conscientes",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { "http-equiv": "X-UA-Compatible", content: "ie=edge" },
+    ],
+  },
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import VueMeta from 'vue-meta';
+import VueMaterial from 'vue-material';
+import 'vue-material/dist/vue-material.min.css';
+import 'vue-material/dist/theme/default-dark.css';
 
-Vue.config.productionTip = false
+Vue.use(VueMeta);
+Vue.use(VueMaterial);
 
 new Vue({
- render: h => h(App),
-}).$mount('#app')
+ render: h => h(App)
+}).$mount('#app');
